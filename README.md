c858f09fc78c387f3e7d7506f09bfbfda900c99b
omareldars <omar.a.eldars@gmail.com>
2021-05-17 15:11:10 +0200

    All Labs

---
+++ README.md
@@ -0,0 +1,1 @@
+# Django-Corse Labs
---
+++ opensource/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from opensource.models import Student, Track
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ('fname', 'lname', 'age', 'student_track')
+        # fields = '__all__'
+        widgets = {
+            'fname': forms.TextInput(attrs={'class': 'form-control'}),
+            'lname': forms.TextInput(attrs={'class': 'form-control'}),
+            'age': forms.NumberInput(attrs={'class': 'form-control'}),
+            'student_track': forms.Select(attrs={'class': 'form-control'})
+        }
+
+
+
+class TrackForm(forms.ModelForm):
+    class Meta:
+        model = Track
+        fields = ('name',)      # comma to tell django its a tuple
